3d46e1ef16f0a1480504f9a04b063f7a7bd40625
kino-3 <y.sugi.cr0204.tech@gmail.com>
2021-09-05 17:40:53 +0900

    feat: check numeric limits

---
+++ sample/check_limits.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    std::cout << "short min:" << std::numeric_limits<short>::min() << endl;
+    std::cout << "short max:" << std::numeric_limits<short>::max() << endl;
+    std::cout << "int min:" << std::numeric_limits<int>::min() << endl;
+    std::cout << "int max:" << std::numeric_limits<int>::max() << endl;
+    std::cout << "long min:" << std::numeric_limits<long>::min() << endl;
+    std::cout << "long max:" << std::numeric_limits<long>::max() << endl;
+    std::cout << "ll min:" << std::numeric_limits<long long>::min() << endl;
+    std::cout << "ll max:" << std::numeric_limits<long long>::max() << endl;
+}
