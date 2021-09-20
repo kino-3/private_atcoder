5f632e6a8c138da5345b1f171955391432e7723a
kino-3 <y.sugi.cr0204.tech@gmail.com>
2021-09-20 12:53:14 +0900

    feat: bellman ford の最小費用流を dijkstra に書き換えた

--- lib_arihon/min_cost_flow/dijkstra_min_cost_flow.cpp
+++ lib_arihon/min_cost_flow/dijkstra_min_cost_flow.cpp
@@ -13,12 +13,19 @@ class MinCostFlow {
     ll V;  // 頂点の個数
     vector<vector<edge>> graph;
     vector<ll> dist;       // dist[v]: v までの最短距離
+    vector<ll> potential;  // source からの最短距離
     vector<ll> prev_edge;  // 最短経路における直前の edge
     vector<ll> prev_node;  // 最短経路における直前の node
     const ll INF = numeric_limits<ll>::max();
 
    public:
-    MinCostFlow(int v) : V(v), graph(v), dist(v), prev_edge(v), prev_node(v) {}
+    MinCostFlow(int v)
+        : V(v),
+          graph(v),
+          dist(v),
+          prev_edge(v),
+          prev_node(v),
+          potential(v, 0) {}
 
     void add_edge(ll from, ll to, ll capacity, ll cost) {
         graph[from].push_back({to, capacity, cost, graph[to].size()});
@@ -29,30 +36,39 @@ class MinCostFlow {
     ll exec(ll source, ll sink, ll flow) {
         ll total_cost = 0;
         while (flow > 0) {
-            // ベルマンフォード法で残余グラフから最短距離を求める
+            // ダイクストラ法で残余グラフから最短距離を求める
+            typedef pair<ll, int> P;  // first は最短距離, second は node
+            priority_queue<P, vector<P>, greater<P>> que;
             fill(dist.begin(), dist.end(), INF);
             dist[source] = 0;
-            bool updated = true;
-            while (updated) {
-                updated = false;
-                for (ll v = 0; v < V; v++) {
-                    if (dist[v] == INF) {
-                        continue;
-                    }
-                    for (ll i = 0; i < graph[v].size(); i++) {
-                        edge& e = graph[v][i];
-                        if (e.capacity > 0 && dist[e.to] > dist[v] + e.cost) {
-                            dist[e.to] = dist[v] + e.cost;
-                            prev_node[e.to] = v;
-                            prev_edge[e.to] = i;
-                            updated = true;
-                        }
+            que.push(P(0, source));
+            while (!que.empty()) {
+                P p = que.top();  // ノード p.second には距離 p.first で到達可能
+                que.pop();
+                ll v = p.second;
+                if (dist[v] < p.first) {
+                    continue;
+                }
+                for (ll i = 0; i < graph[v].size(); i++) {
+                    edge& e = graph[v][i];
+                    // potential 下での edge の cost
+                    ll new_cost = e.cost + potential[v] - potential[e.to];
+                    if (e.capacity > 0 && dist[e.to] > dist[v] + new_cost) {
+                        dist[e.to] = dist[v] + new_cost;
+                        prev_node[e.to] = v;
+                        prev_edge[e.to] = i;
+                        que.push(P(dist[e.to], e.to));
                     }
                 }
             }
 
             if (dist[sink] == INF) {
                 return -1;  // flow を流すことは出来ない
+            }
+
+            // potential の更新
+            for (ll v = 0; v < V; v++) {
+                potential[v] += dist[v];
             }
 
             // 最短距離に目一杯流す
@@ -62,7 +78,7 @@ class MinCostFlow {
                                   graph[prev_node[v]][prev_edge[v]].capacity);
             }
             flow -= actual_flow;  // 残り流さないといけない流量
-            total_cost += actual_flow * dist[sink];
+            total_cost += actual_flow * potential[sink];
 
             // 残余グラフの更新
             for (ll v = sink; v != source; v = prev_node[v]) {  // 逆向きに辿る
