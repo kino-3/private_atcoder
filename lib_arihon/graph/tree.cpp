80f2ebcc4ca0e75799e19713353cb5c57957c84f
kino-3 <y.sugi.cr0204.tech@gmail.com>
2021-10-15 00:03:26 +0900

    feat: ある点からの深さを測る機能を追加した

--- lib_arihon/graph/tree.cpp
+++ lib_arihon/graph/tree.cpp
@@ -10,6 +10,9 @@ class Tree {
     vector<ll> parent;        // 頂点の親ノード
     vector<ll> subtree_size;  // subtree_size
     vector<ll> centroids;     // 木の重心
+    vector<ll> depth;         // ある点からの距離
+    vector<ll> sub;           // どの sub の部分木か
+    vector<ll> sub_list;      // sub の種類を集めたもの
 
     Tree(ll v) : V(v), graph(v), parent(v, -1), subtree_size(v, -1) {}
 
@@ -33,6 +36,31 @@ class Tree {
         }
         subtree_size[v] = size;
         return size;
+    }
+
+    // depth と sub に以下の情報を格納する
+    // depth[v] == (root から vの距離)
+    // sub[v] == (rootからvまでの経路でrootに隣接する頂点)
+    // sub_list == (sub の集合)
+    // 外部から呼び出すときは calc_subtree_size(root) とすること
+    ll calc_depth(ll v, ll parent_of_v = -1) {
+        if (parent_of_v == -1) {
+            calc_subtree_size(v);
+            sub_list.clear();
+            depth[v] = 0;
+            for (const auto& child : graph[v]) {
+                // child は root に隣接する node
+                depth[child] = 1;
+                sub[child] = child;
+                sub_list.push_back(child);
+            }
+        } else {
+            for (const auto& child : graph[v]) {
+                if (child == parent_of_v) continue;  // 隣接リストなので
+                depth[child] = depth[v] + 1;
+                sub[child] = sub[v];
+            }
+        }
     }
 
     // calc_subtree_size + 重心分解
