949ffaef0465de5817c039e8d6197270b11ebded
kino-3 <y.sugi.cr0204.tech@gmail.com>
2021-10-26 01:35:15 +0900

    feat: 033

---
+++ atcoder/other/typical90/033.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define REP(var, n) for (decltype(n) var = 0; var < (n); var++)
+#define REP_R(var, n) \
+    for (auto var = (n)-1; var != static_cast<decltype(var)>(-1); var--)
+#define FOR(var, a, b) for (auto var = (a); var < (b); var++)
+#define FOR_R(var, a, b) for (auto var = (b - 1); var > (a - 1); var--)
+#define ALL(c) std::begin(c), std::end(c)
+
+using namespace std;
+using ll = long long;
+
+// REP(idx, 3) { cout << idx; }  // 012
+// REP_R(idx, 3) { cout << idx; }  // 210
+// FOR(idx, 4, 7) { cout << idx; }  // 456
+// FOR_R(idx, 4, 7) { cout << idx; }  // 654
+// sort(ALL(v));
+
+ll H, W;
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+
+    cin >> H >> W;
+    if (H == 1 || W == 1) {
+        cout << H * W << endl;
+    } else {
+        cout << ((H + 1) / 2) * ((W + 1) / 2) << endl;
+    }
+}
