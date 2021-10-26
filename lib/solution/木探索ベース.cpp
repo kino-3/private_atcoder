99ae094f2de5a5c057658d909ba7704f3b628db3
kino-3 <y.sugi.cr0204.tech@gmail.com>
2021-10-27 00:04:59 +0900

    feat: 039

---
+++ lib/solution/木探索ベース.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define REP(var, n) for (decltype(n) var = 0; var < (n); var++)
+#define REP_R(var, n) \
+    for (auto var = (n)-1; var != static_cast<decltype(var)>(-1); var--)
+#define FOR(var, a, b) for (auto var = (a); var < (b); var++)
+#define FOR_R(var, a, b) for (auto var = (b - 1); var > (a - 1); var--)
+#define ALL(c) std::begin(c), std::end(c)
+
+using namespace std;
+using ll = long long;
+
+// REP(idx, 3) { cout << idx; }  // 012
+// REP_R(idx, 3) { cout << idx; }  // 210
+// FOR(idx, 4, 7) { cout << idx; }  // 456
+// FOR_R(idx, 4, 7) { cout << idx; }  // 654
+// sort(ALL(v));
+
+template <typename T>
+void print_v(const vector<T> vec) {
+    cout << "size: " << vec.size() << endl;
+    cout << "[";
+    for (auto&& item : vec) {
+        cout << item << ",";
+    }
+    cout << "]" << endl;
+}
+
+class Tree {
+   public:
+    ll V;                         // 頂点の個数
+    vector<vector<ll>> graph;     // 隣接リスト
+    vector<ll> parent;            // 親ノード
+    vector<vector<ll>> children;  // 子ノード
+    vector<ll> size;  // 自身を root とする部分木の大きさ
+
+    Tree(ll v) : V(v), graph(v), parent(v), children(v), size(v) {}
+
+    void add_edge(ll n1, ll n2) {
+        graph[n1].push_back(n2);
+        graph[n2].push_back(n1);
+    }
+
+    // 頂点 v で吊ったときの木探索
+    ll exec(ll v, ll parent_of_v = -1) {
+        // if (parent_of_v == -1) {}
+        parent[v] = parent_of_v;
+        ll s = 1;
+        for (const auto& child : graph[v]) {
+            if (child == parent_of_v) continue;
+            s += exec(child, v);
+            children[v].push_back(child);
+        }
+        size[v] = s;
+        return s;
+    }
+};
+
+// input
+// N
+// a_1   b_1
+// a_2   b_2
+//  :
+// a_N-1 a_N-1
+
+ll N, i, j, k;
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+
+    cin >> N;
+    Tree tree = Tree(N);
+    REP(i, N - 1) {
+        cin >> j >> k;
+        tree.add_edge(j - 1, k - 1);
+    }
+    tree.exec(0);
+}
