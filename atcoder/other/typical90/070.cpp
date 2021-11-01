70470a9431bb02fc706946e17324a6ba18b61368
kino-3 <y.sugi.cr0204.tech@gmail.com>
2021-11-02 01:06:37 +0900

    feat: 070

---
+++ atcoder/other/typical90/070.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define REP(var, n) for (decltype(n) var = 0; var < (n); var++)
+#define REP_R(var, n) \
+    for (auto var = (n)-1; var != static_cast<decltype(var)>(-1); var--)
+#define FOR(var, a, b) for (auto var = (a); var < (b); var++)
+#define FOR_R(var, a, b) for (auto var = (b - 1); var > (a - 1); var--)
+#define ALL(c) std::begin(c), std::end(c)
+
+using namespace std;
+using ll = long long;
+
+// REP(idx, 3) { cout << idx; }  // 012
+// REP_R(idx, 3) { cout << idx; }  // 210
+// FOR(idx, 4, 7) { cout << idx; }  // 456
+// FOR_R(idx, 4, 7) { cout << idx; }  // 654
+// sort(ALL(v));
+
+ll mod = 1000000007;
+ll N, i, j, k, l;
+vector<ll> X, Y;
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+
+    cin >> N;
+    REP(i, N) {
+        cin >> j >> k;
+        X.push_back(j);
+        Y.push_back(k);
+    }
+    sort(ALL(X));
+    sort(ALL(Y));
+    ll x = X[N / 2];
+    ll y = Y[N / 2];
+    ll ans = 0;
+    REP(i, N) { ans += abs(X[i] - x) + abs(Y[i] - y); }
+    cout << ans << endl;
+}
