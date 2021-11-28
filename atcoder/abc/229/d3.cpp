6e5df3d5e927acb176bcf45d3d5e5121860898cb
kino-3 <y.sugi.cr0204.tech@gmail.com>
2021-11-28 15:21:39 +0900

    feat: abc 229 d 尺取

---
+++ atcoder/abc/229/d3.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#define REP(var, n) for (decltype(n) var = 0; var < (n); var++)
+#define REP_R(var, n) \
+    for (auto var = (n)-1; var != static_cast<decltype(var)>(-1); var--)
+#define FOR(var, a, b) for (auto var = (a); var < (b); var++)
+#define FOR_R(var, a, b) for (auto var = (b - 1); var > (a - 1); var--)
+#define ALL(c) std::begin(c), std::end(c)
+
+using namespace std;
+using ll = long long;
+
+// REP(idx, 3) { cout << idx; }  // 012
+// REP_R(idx, 3) { cout << idx; }  // 210
+// FOR(idx, 4, 7) { cout << idx; }  // 456
+// FOR_R(idx, 4, 7) { cout << idx; }  // 654
+// sort(ALL(v));
+
+template <typename T>
+void debug_print(const T item) {
+    cout << item << endl;
+}
+ll DEBUG_PRINT_COUNT = 0;
+void debug_print_count() {
+    cout << "debug: " << DEBUG_PRINT_COUNT << endl;
+    DEBUG_PRINT_COUNT++;
+    assert(DEBUG_PRINT_COUNT < 10);
+}
+template <typename T>
+void print_v(const vector<T> vec) {
+    cout << "[";
+    for (auto &&item : vec) {
+        cout << item << ",";
+    }
+    cout << "]" << endl;
+}
+template <typename T>
+void print_vv(const vector<T> vec) {
+    for (auto &&item : vec) {
+        print_v(item);
+    }
+}
+template <typename K, typename V>
+void print_map(const map<K, V> dict) {
+    for (const auto v : dict) {
+        cout << v.first << ":" << v.second << ", ";
+    }
+    cout << endl;
+}
+template <typename T>
+void print_set(const set<T> data) {
+    for (const auto v : data) {
+        cout << v << ", ";
+    }
+    cout << endl;
+}
+template <typename T1, typename T2>
+void print_pair(const pair<T1, T2> data) {
+    cout << "(" << data.first << "," << data.second << ")";
+    // cout << endl;
+}
+template <typename T1, typename T2, typename T3>
+void print_tuple(const tuple<T1, T2, T3> data) {
+    cout << "(";
+    cout << get<0>(data) << "," << get<1>(data) << "," << get<2>(data);
+    cout << ")";
+    // cout << endl;
+}
+
+ll K, i, j, k, l;
+string S;
+vector<ll> A;
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+
+    cin >> S;
+    cin >> K;
+
+    ll tmp = 0;
+    REP(i, S.size()) {
+        if (S[i] == '.') tmp++;
+        A.push_back(tmp);
+    }
+
+    ll ans = 0;
+    // 条件を満たす最長を考えるときの尺取り
+    // TODO: 最短を考える場合は right を 1 ずつ進めて, left
+    // を条件を満たさなくなるまで進める
+    ll right = 0;
+    for (ll left = 0; left < S.size(); left++) {
+        while (A[right] - (left == 0 ? 0 : A[left - 1]) <= K) {
+            ans = max(ans, right + 1 - left);
+            if (right == S.size() - 1) {
+                break;
+            } else {
+                right++;
+            }
+        }
+    }
+    cout << ans << endl;
+}
