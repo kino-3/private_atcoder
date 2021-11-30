0f96f8aa014e6a8e9125b2b9ba08ffa1ff94a93f
kino-3 <y.sugi.cr0204.tech@gmail.com>
2021-11-30 22:20:59 +0900

    feat: abc 229 f

--- atcoder/abc/229/f.cpp
+++ atcoder/abc/229/f.cpp
@@ -41,14 +41,14 @@ void print_vv(const vector<T> vec) {
 }
 template <typename K, typename V>
 void print_map(const map<K, V> dict) {
-    for (const auto v: dict) {
+    for (const auto v : dict) {
         cout << v.first << ":" << v.second << ", ";
     }
     cout << endl;
 }
 template <typename T>
 void print_set(const set<T> data) {
-    for (const auto v: data) {
+    for (const auto v : data) {
         cout << v << ", ";
     }
     cout << endl;
@@ -66,93 +66,6 @@ void print_tuple(const tuple<T1, T2, T3> data) {
     // cout << endl;
 }
 
-struct edge {
-    ll to;
-    ll capacity;
-    ll rev;  // graph[to][rev] が逆辺を表す
-};
-
-class Dinic {
-    // graph[i] の要素 e は, i から e.to へあと e.capacity
-    // だけ流せるという情報をもつ。
-    // また, graph[e.to][e.rev] は e の逆辺である。
-    vector<vector<edge>> graph;
-    const ll INF = numeric_limits<ll>::max();
-    vector<ll> level;
-    vector<ll> iter;
-    ll source;  // 流入口
-    ll sink;    // 流出口
-
-   public:
-    Dinic(ll v) : graph(v), source(0), sink(v - 1), level(v, -1), iter(v, 0) {}
-
-    void add_edge(ll from, ll to, ll capacity) {
-        graph[from].push_back({to, capacity, graph[to].size()});
-        graph[to].push_back({from, 0, graph[from].size() - 1});  // 逆辺
-    }
-
-    void bfs() {
-        // level に以下の情報を格納する
-        // level[v] は source から v までに経由する(最小)ノード数
-        // (ただし, 正の capacity の辺 (及び逆辺)のみ通過できる)
-        // 到達不可能な場合は -1 とする
-        std::fill(level.begin(), level.end(), -1);
-        queue<ll> que;
-        level[source] = 0;
-        que.push(source);
-        while (!que.empty()) {
-            ll node = que.front();
-            que.pop();
-            for (const auto& edge : graph[node]) {
-                if (edge.capacity > 0 && level[edge.to] < 0) {
-                    // edge.to はまだ探索していない到達可能ノード
-                    level[edge.to] = level[node] + 1;
-                    que.push(edge.to);
-                }
-            }
-        }
-    }
-
-    ll dfs(ll node, ll flow) {
-        // node に flow だけ流し込もうとしたとき, 実際はどれだけ流れるか
-        // 戻り値は flow 以下の値である
-        if (node == sink) {
-            return flow;
-        }
-        for (ll& i = iter[node]; i < graph[node].size(); i++) {
-            // 2 回目以降に dfs が呼び出されたときは, 続きから探索する
-            // iter[node] は i++ で書き換わる
-            edge& edge = graph[node][i];
-            if (edge.capacity > 0 && level[node] < level[edge.to]) {
-                ll actual_flow = dfs(edge.to, min(flow, edge.capacity));
-                if (actual_flow > 0) {
-                    edge.capacity -= actual_flow;
-                    graph[edge.to][edge.rev].capacity += actual_flow;
-                    return actual_flow;
-                }
-            }
-        }
-        return 0;
-    }
-
-    ll exec(ll source_node, ll sink_node) {
-        source = source_node;
-        sink = sink_node;
-        ll max_flow = 0;
-        while (true) {
-            bfs();
-            if (level[sink] < 0) {
-                return max_flow;
-            }
-            std::fill(iter.begin(), iter.end(), 0);
-            ll flow;
-            while ((flow = dfs(source, INF)) > 0) {
-                max_flow += flow;
-            }
-        }
-    }
-};
-
 const ll mod = 998244353;
 ll N, i, j, k, l;
 vector<ll> A, B;
@@ -161,10 +74,42 @@ int main() {
     std::cin.tie(nullptr);
     std::ios::sync_with_stdio(false);
 
-    cin >> N >> M;
-    Graph graph = Graph(N);
-    REP(i, M) {
-        cin >> j >> k;
-        graph.add_undirected_edge(j - 1, k - 1);
+    cin >> N;
+    A.resize(N);
+    REP(i, N) { cin >> A[i]; }
+    B.resize(N);
+    REP(i, N) { cin >> B[i]; }
+
+    ll same, diff;
+
+    // c0 == c1 の場合
+    same = A[0];
+    diff = 10000000000000000;
+    FOR(i, 1, N) {  // i = 1, ..., N-1
+        ll tmp_same = min(same + B[i - 1], diff) + A[i];
+        ll tmp_diff = min(same, diff + B[i - 1]);
+        same = tmp_same;
+        diff = tmp_diff;
     }
+    same += B[N - 1];
+
+    ll ans = min(same, diff);
+
+    // c0 != c1 の場合
+    same = 10000000000000000;
+    diff = 0;
+    FOR(i, 1, N) {  // i = 1, ..., N-1
+        ll tmp_same = min(same + B[i - 1], diff) + A[i];
+        ll tmp_diff = min(same, diff + B[i - 1]);
+        same = tmp_same;
+        diff = tmp_diff;
+        // debug_print(same);
+        // debug_print(diff);
+        // debug_print_count();
+    }
+    diff += B[N - 1];
+
+    ans = min(ans, min(same, diff));
+
+    cout << ans << endl;
 }
