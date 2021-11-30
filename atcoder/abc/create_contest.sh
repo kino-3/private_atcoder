48ace0edbc655f339e18cca1cf1530c5aa29de99
kino-3 <y.sugi.cr0204.tech@gmail.com>
2021-11-30 22:40:30 +0900

    feat: コンテスト用ファイルを生成するスクリプトを作成した

---
+++ atcoder/abc/create_contest.sh
@@ -0,0 +1,13 @@
+cd `dirname $0`
+
+mkdir $1
+cd $1
+touch a.cpp
+touch b.cpp
+touch c.cpp
+touch d.cpp
+touch e.cpp
+touch f.cpp
+touch g.cpp
+touch h.cpp
+echo -e "# ABC $1\n\n## 重要メモ\n\n## E" > README.md
