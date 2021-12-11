4b7e89d907ca71e4d70fe43a310de4ba750bb9e2
kino-3 <y.sugi.cr0204.tech@gmail.com>
2021-12-11 21:42:21 +0900

    tmp

--- atcoder/abc/231/d.cpp
+++ atcoder/abc/231/d.cpp
@@ -74,6 +74,40 @@ void print_vp(const vector<pair<T1, T2>> vec) {
     }
 }
 
+class UnionFind {
+   public:
+    // 根ノードなら -(データ数)
+    // それ以外なら親ノード
+    // が格納される
+    std::vector<ll> data;
+
+    UnionFind(ll size) : data(size, -1) {}
+
+    // 集合を併合する
+    // 併合できたかを返す(すでに同じ木に属するなら併合不要なのでfalse)
+    bool unite(ll x, ll y) {
+        x = root(x);
+        y = root(y);
+        if (x != y) {
+            if (data[y] < data[x]) std::swap(x, y);
+            // 要素数の大きな方(x)へyを合併する
+            data[x] += data[y];
+            data[y] = x;
+        }
+        return x != y;
+    }
+
+    // 同じ木に属するか
+    bool find(ll x, ll y) { return root(x) == root(y); }
+
+    // 属する木の根を返す
+    // (その過程で根まで辿ったノードを根に付ける)
+    ll root(ll x) { return (data[x] < 0) ? x : data[x] = root(data[x]); }
+
+    // 集合の要素数を返す
+    ll size(ll x) { return -data[root(x)]; }
+};
+
 const ll mod = 998244353;
 ll N, M, Q, i, j, k, l;
 vector<ll> A, B;
@@ -88,10 +122,17 @@ int main() {
     REP(i, M) { cin >> A[i] >> B[i]; }
     bool ans = true;
 
-    vector<set<ll>> d(N);
+    set<pair<ll, ll>> pairing;
     REP(i, M) {
-        d[A[i] - 1].insert(B[i] - 1);
-        d[B[i] - 1].insert(A[i] - 1);
+        pairing.insert({min(A[i] - 1, B[i] - 1), max(A[i] - 1, B[i] - 1)});
+    }
+
+    vector<set<ll>> d(N);
+    UnionFind uf = UnionFind(N);
+    for (auto v : pairing) {
+        d[v.first].insert(v.second);
+        d[v.second].insert(v.first);
+        if (!uf.unite(v.first, v.second)) ans = false;
     }
 
     REP(i, N) {
