c5622ddba1246e867d9faf676f1cba801c86f8b6
kino-3 <y.sugi.cr0204.tech@gmail.com>
2022-01-22 22:13:40 +0900

    WA

---
+++ atcoder/arc/133/c.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+#define REP(var, n) for (decltype(n) var = 0; var < (n); var++)
+#define REP_R(var, n) \
+    for (auto var = (n)-1; var != static_cast<decltype(var)>(-1); var--)
+#define FOR(var, a, b) for (auto var = (a); var < (b); var++)
+#define FOR_R(var, a, b) for (auto var = (b - 1); var > (a - 1); var--)
+#define ALL(c) std::begin(c), std::end(c)
+
+using namespace std;
+using ll = long long;
+
+// REP(idx, 3) { cout << idx; }  // 012
+// REP_R(idx, 3) { cout << idx; }  // 210
+// FOR(idx, 4, 7) { cout << idx; }  // 456
+// FOR_R(idx, 4, 7) { cout << idx; }  // 654
+// sort(ALL(v));
+
+#ifdef _DEBUG
+void debug_print() { cout << endl; }
+template <class Head, class... Tail>
+void debug_print(Head &&head, Tail &&...tail) {
+    std::cout << head << ", ";
+    debug_print(std::forward<Tail>(tail)...);
+}
+ll DEBUG_PRINT_COUNT = 0;
+void debug_print_count() {
+    cout << "debug: " << DEBUG_PRINT_COUNT << endl;
+    DEBUG_PRINT_COUNT++;
+    assert(DEBUG_PRINT_COUNT < 10);
+}
+template <typename T>
+void print_v(const vector<T> vec) {
+    cout << "[";
+    for (auto &&item : vec) {
+        cout << item << ",";
+    }
+    cout << "]" << endl;
+}
+template <typename T>
+void print_vv(const vector<T> vec) {
+    for (auto &&item : vec) {
+        print_v(item);
+    }
+}
+template <typename K, typename V>
+void print_map(const map<K, V> dict) {
+    for (const auto v : dict) {
+        cout << v.first << ":" << v.second << ", ";
+    }
+    cout << endl;
+}
+template <typename T>
+void print_set(const set<T> data) {
+    for (const auto v : data) {
+        cout << v << ", ";
+    }
+    cout << endl;
+}
+template <typename T1, typename T2>
+void print_pair(const pair<T1, T2> data) {
+    cout << "(" << data.first << "," << data.second << ")";
+    // cout << endl;
+}
+template <typename T1, typename T2, typename T3>
+void print_tuple(const tuple<T1, T2, T3> data) {
+    cout << "(";
+    cout << get<0>(data) << "," << get<1>(data) << "," << get<2>(data);
+    cout << ")";
+    // cout << endl;
+}
+template <typename T1, typename T2>
+void print_vp(const vector<pair<T1, T2>> vec) {
+    for (auto &&item : vec) {
+        print_pair(item);
+    }
+}
+#else
+void debug_print() {}
+template <class Head, class... Tail>
+void debug_print(Head&& head, Tail&&... tail) {}
+void debug_print_count() {}
+template <typename T>
+void print_v(const vector<T> vec) {}
+template <typename T>
+void print_vv(const vector<T> vec) {}
+template <typename K, typename V>
+void print_map(const map<K, V> dict) {}
+template <typename T>
+void print_set(const set<T> data) {}
+template <typename T1, typename T2>
+void print_pair(const pair<T1, T2> data) {}
+template <typename T1, typename T2, typename T3>
+void print_tuple(const tuple<T1, T2, T3> data) {}
+template <typename T1, typename T2>
+void print_vp(const vector<pair<T1, T2>> vec) {}
+#endif
+
+const ll mod = 998244353;  // 1000000007;
+ll H, W, K, N, M, Q, i, j, k, l;
+vector<ll> A, B, AA, BB;
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+
+    cin >> H >> W >> K;
+    A.resize(H);
+    B.resize(W);
+    REP(i, H) { cin >> A[i]; }
+    REP(i, W) { cin >> B[i]; }
+    AA.resize(H);
+    BB.resize(W);
+    ll a = 0;
+    ll b = 0;
+    REP(i, H) {
+        AA[i] = (K * W + K - W - A[i]) % K;
+        a += AA[i];
+    }
+    REP(i, W) {
+        BB[i] = (K * H + K - H - B[i]) % K;
+        b += BB[i];
+    }
+    // print_v(AA);
+    // print_v(BB);
+    if ((a % K) != (b % K)) {
+        cout << -1 << endl;
+    } else {
+        cout << H * W * (K - 1) - max(a, b) << endl;
+    }
+}
