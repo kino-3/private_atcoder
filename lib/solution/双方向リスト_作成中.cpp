9b92f633b86f965e39d1b67e7c59c76048afd7fa
kino-3 <y.sugi.cr0204.tech@gmail.com>
2022-02-20 00:44:05 +0900

    feat: 双方向リンク(作成中)

---
+++ lib/solution/双方向リスト_作成中.cpp
@@ -0,0 +1,174 @@
+#include <bits/stdc++.h>
+#define REP(var, n) for (decltype(n) var = 0; var < (n); var++)
+#define REP_R(var, n) \
+    for (auto var = (n)-1; var != static_cast<decltype(var)>(-1); var--)
+#define FOR(var, a, b) for (auto var = (a); var < (b); var++)
+#define FOR_R(var, a, b) for (auto var = (b - 1); var > (a - 1); var--)
+#define ALL(c) std::begin(c), std::end(c)
+
+using namespace std;
+using ll = long long;
+
+// REP(idx, 3) { cout << idx; }  // 012
+// REP_R(idx, 3) { cout << idx; }  // 210
+// FOR(idx, 4, 7) { cout << idx; }  // 456
+// FOR_R(idx, 4, 7) { cout << idx; }  // 654
+// sort(ALL(v));
+
+#ifdef _DEBUG
+void debug_print() { cout << endl; }
+template <class Head, class... Tail>
+void debug_print(Head &&head, Tail &&...tail) {
+    std::cout << head << ", ";
+    debug_print(std::forward<Tail>(tail)...);
+}
+ll DEBUG_PRINT_COUNT = 0;
+void debug_print_count() {
+    cout << "debug: " << DEBUG_PRINT_COUNT << endl;
+    DEBUG_PRINT_COUNT++;
+    assert(DEBUG_PRINT_COUNT < 10);
+}
+template <typename T>
+void print_v(const vector<T> vec) {
+    cout << "[";
+    for (auto &&item : vec) {
+        cout << item << ",";
+    }
+    cout << "]" << endl;
+}
+template <typename T>
+void print_vv(const vector<T> vec) {
+    for (auto &&item : vec) {
+        print_v(item);
+    }
+}
+template <typename K, typename V>
+void print_map(const map<K, V> dict) {
+    for (const auto v : dict) {
+        cout << v.first << ":" << v.second << ", ";
+    }
+    cout << endl;
+}
+template <typename T>
+void print_set(const set<T> data) {
+    for (const auto v : data) {
+        cout << v << ", ";
+    }
+    cout << endl;
+}
+template <typename T1, typename T2>
+void print_pair(const pair<T1, T2> data) {
+    cout << "(" << data.first << "," << data.second << ")";
+    // cout << endl;
+}
+template <typename T1, typename T2, typename T3>
+void print_tuple(const tuple<T1, T2, T3> data) {
+    cout << "(";
+    cout << get<0>(data) << "," << get<1>(data) << "," << get<2>(data);
+    cout << ")";
+    // cout << endl;
+}
+template <typename T1, typename T2>
+void print_vp(const vector<pair<T1, T2>> vec) {
+    for (auto &&item : vec) {
+        print_pair(item);
+    }
+}
+#else
+void debug_print() {}
+template <class Head, class... Tail>
+void debug_print(Head &&head, Tail &&...tail) {}
+void debug_print_count() {}
+template <typename T>
+void print_v(const vector<T> vec) {}
+template <typename T>
+void print_vv(const vector<T> vec) {}
+template <typename K, typename V>
+void print_map(const map<K, V> dict) {}
+template <typename T>
+void print_set(const set<T> data) {}
+template <typename T1, typename T2>
+void print_pair(const pair<T1, T2> data) {}
+template <typename T1, typename T2, typename T3>
+void print_tuple(const tuple<T1, T2, T3> data) {}
+template <typename T1, typename T2>
+void print_vp(const vector<pair<T1, T2>> vec) {}
+#endif
+
+struct Node {
+    Node *prev;
+    Node *next;
+    ll value;
+};
+
+class LinkedList {
+   public:
+    Node *front;  // 番兵
+    Node *back;   // 番兵
+    ll size;
+
+    LinkedList() : size(0) {
+        (*front).next = back;
+        (*back).prev = front;
+    }
+
+    void set_vector(vector<ll> vec) {
+        for (auto v : vec) push_back(v);
+    }
+
+    void push_back(ll val) {
+        size++;
+        Node new_node;
+        auto tmp_last = (*back).prev;
+        new_node.value = val;
+        new_node.prev = tmp_last;
+        new_node.next = back;
+        (*back).prev = &new_node;
+        (*tmp_last).next = &new_node;
+    }
+
+    vector<ll> to_vector() {
+        vector<ll> res;
+        Node *tmp = front;
+        ll loop_count;
+        REP(loop_count, size) {
+            tmp = (*tmp).next;
+            res.push_back((*tmp).value);
+        }
+        return res;
+    }
+};
+
+const ll mod = 998244353;  // 1000000007;
+ll N, M, Q, i, j, k, l;
+vector<ll> A, B;
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+
+    cin >> N;
+    A.resize(N);
+    B.resize(N);
+    REP(i, N) {
+        cin >> A[i];
+        cin >> B[i];
+    }
+
+    LinkedList la = LinkedList();
+    la.push_back(1);
+    la.push_back(2);
+    la.push_back(2);
+    la.push_back(2);
+    la.push_back(2);
+    // debug_print("hoge");
+    la.push_back(2);
+    debug_print_count();
+    // la.push_back(3);
+    // debug_print_count();
+    // print_v(la.to_vector());
+    // la.set_vector(A);
+    // print_v(la.to_vector());
+    // LinkedList lb = LinkedList();
+    // lb.set_vector(B);
+}
